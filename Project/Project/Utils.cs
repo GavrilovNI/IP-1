4f6a2752e665d1874d050971756702fe93063ecf
Никита Гаврилов <pelmeshka1110@gmail.com>
2020-10-06 14:52:48 +0300

    first

---
+++ Project/Project/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP1
+{
+    namespace Imaging
+    {
+        public static class Utils
+        {
+            public static float Clamp(float value, float min, float max)
+            {
+                if (min > max)
+                    throw new Exception("Clamp error min>max");
+
+                if (value < min)
+                    return min;
+                if (value > max)
+                    return max;
+                return value;
+            }
+
+        }
+    }
+}
