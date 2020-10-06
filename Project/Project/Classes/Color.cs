70672561b940f893c029182767f8de3aa29263e4
Никита Гаврилов <pelmeshka1110@gmail.com>
2020-10-06 21:19:40 +0300

    fixes

--- Project/Project/Classes/Color.cs
+++ Project/Project/Classes/Color.cs
@@ -15,7 +15,7 @@ namespace IP1
             public static Color Red => new Color(255, 0, 0);
             public static Color Green => new Color(0, 255, 0);
             public static Color Blue => new Color(0, 0, 255);
-            public static Color Fuchsia => new Color(255, 0, 255);
+            public static Color Magenta => new Color(255, 0, 255);
             public static Color Yellow => new Color(255, 255, 0);
             public static Color Cyan => new Color(0, 255, 255);
 
