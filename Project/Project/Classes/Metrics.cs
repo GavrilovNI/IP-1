1d98c30163d59ae33138aa0cd37c8a4ab645f717
SozinovAP <alexsozinov73@gmail.com>
2020-10-06 17:32:22 +0300

    Add metrics

---
+++ Project/Project/Classes/Metrics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP1.Classes
+{
+    class Metrics
+    {
+    }
+}
