83750a5c085d6fd32f167a2c4a0b79bf3933360e
SozinovAP <alexsozinov73@gmail.com>
2020-10-16 11:12:44 +0300

    Interface V1.0

--- Project/Project/MainWindow.xaml.cs
+++ Project/Project/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using IP1.Imaging.Filters;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -23,33 +25,101 @@ namespace IP1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Imaging.Image myImage;
+
         public MainWindow()
         {
 
             InitializeComponent();
-            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.NearestNeighbor);
+            RenderOptions.SetBitmapScalingMode(CustomIm, BitmapScalingMode.NearestNeighbor);
+            RenderOptions.SetBitmapScalingMode(OpenCVIm, BitmapScalingMode.NearestNeighbor);
+            RenderOptions.SetBitmapScalingMode(OriginalIm, BitmapScalingMode.NearestNeighbor);
 
-            byte[] arr = new byte[3] { 1, 2, 3 };
+            /*Imaging.Image myImage = Imaging.Image.Load("image");
 
-            Imaging.Image myImage = new Imaging.Image(3, 2);
+            //myImage = new FilterGrayScale(FilterGrayScale.GrayScaleType.Gimp).Run(myImage);
 
-
-
-            myImage[0, 0] = Imaging.Color.Red;
-            myImage[0, 1] = Imaging.Color.Green;
-            myImage[0, 2] = Imaging.Color.Blue;
-            myImage[1, 0] = Imaging.Color.Yellow;
-            myImage[1, 1] = Imaging.Color.Cyan;
-            myImage[1, 2] = Imaging.Color.Fuchsia;
+            ConvertOpenCV opcv = new ConvertOpenCV();
+            System.Drawing.Image SecondIm = opcv.ConvertToGray(Imaging.Image.Load("image"));
 
 
-            myImage = Imaging.Image.Load("image");
+            image2.Source = Utils.ImageToBitmapSource(myImage);
+            
+            var memory = new MemoryStream();
+                SecondIm.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
 
-            myImage = new FilterGrayScale(FilterGrayScale.GrayScaleType.Gimp).Run(myImage);
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            image.Source = bitmapImage;
 
+            CustomIm.Source = Utils.ImageToBitmapSource(myImage);
+            System.Drawing.Image three = opcv.RGB2HSV(Imaging.Image.Load("image"));
 
+            var memory = new MemoryStream();
+            three.Save(memory, ImageFormat.Png);
+            memory.Position = 0;
 
-            image.Source = Utils.ImageToBitmapSource(myImage);
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = memory;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            OpenCVIm.Source = bitmapImage;*/
+        }
+
+        private void ButtonLoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                //MainWindow.myImage = Imaging.Image.Load(filename);
+                Imaging.Image myImage = Imaging.Image.Load("image");
+                OriginalIm.Source = Utils.ImageToBitmapSource(myImage);
+            }
+        }
+
+        public void RunConvertToGrayScale()
+        {
+            //Add Timer
+            //офисная техника. Посредник
+        }
+
+        public void RunConvertRGBToHSV()
+        {
+            //Add Timer
+
+        }
+
+        public void RunConvertHSVToRGB()
+        {
+            //Add Timer
+
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            if (radioButtonGrayScale.IsChecked == true)
+            {
+                RunConvertToGrayScale();
+            }
+            else if (radioButtonRGB2HSV.IsChecked == true)
+            {
+                RunConvertRGBToHSV();
+            }
+            else if (radioButtonHSV2RGB.IsChecked == true)
+            {
+                RunConvertHSVToRGB();
+            }
         }
     }
 }
