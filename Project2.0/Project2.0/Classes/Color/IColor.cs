a1080fb698dc808fec680c65fcc92519200c2c69
Лёня <lioniavlasov@gmail.com>
2020-10-17 02:36:09 +0300

    That's how bugs look

---
+++ Project2.0/Project2.0/Classes/Color/IColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IP1.Imaging.ColorNS
+{
+    public interface IColor
+    {
+        static IColor White { get; }
+        static IColor Black { get; }
+        static IColor Red { get; }
+        static IColor Green { get; }
+        static IColor Blue { get; }
+        static IColor Magenta { get; }
+        static IColor Yellow { get; }
+        static IColor Cyan { get; }
+    }
+}
